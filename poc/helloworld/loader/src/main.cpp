483acd21d29d0cb32514523d578354942e547198
xjt <969211735@qq.com>
2026-05-07 14:53:41 +0800

    Update

--- poc/helloworld/loader/src/main.cpp
+++ poc/helloworld/loader/src/main.cpp
@@ -31,15 +31,15 @@ typedef uint64_t QWORD;
 
 typedef struct _IMAGE_DOS_HEADER
 {
-	WORD e_magic;
-	BYTE reserved[58];
-	DWORD e_lfanew;
+	WORD e_magic;				// File First Byte: "MZ" (0x5A4D i.e. littleend 0x4D5A)
+	BYTE reserved[58];			// Historical waste
+	DWORD e_lfanew;				// Point to real PE header (Long File Address New), begin at 0x3C (Decimal 59)
 } IMAGE_DOS_HEADER;
 
 typedef struct _IMAGE_FILE_HEADER
 {
 	WORD Machine;
-	WORD NumberOfSections;
+	WORD NumberOfSections;			// Code block, data block etc.
 	DWORD TimeDateStamp;
 	DWORD PointerToSymbolTable;
 	DWORD NumberOfSymbols;
@@ -73,7 +73,7 @@ typedef struct _IMAGE_OPTIONAL_HEADER64
 	WORD MajorSubsystemVersion;
 	WORD MinorSubsystemVersion;
 	DWORD Win32VersionValue;
-	DWORD SizeOfImage;
+	DWORD SizeOfImage;								// How many memory need to mmap in Linux
 	DWORD SizeOfHeaders;
 	DWORD CheckSum;
 	WORD Subsystem;
@@ -84,23 +84,32 @@ typedef struct _IMAGE_OPTIONAL_HEADER64
 	QWORD SizeOfHeapCommit;
 	DWORD LoaderFlags;
 	DWORD NumberOfRvaAndSizes;
-	IMAGE_DATA_DIRECTORY DataDirectory[16];
+	IMAGE_DATA_DIRECTORY DataDirectory[16];			// Where is Export Table in Portable Executable
 } IMAGE_OPTIONAL_HEADER64;
 
 typedef struct _IMAGE_NT_HEADERS64
 {
-	DWORD Signature;
-	IMAGE_FILE_HEADER FileHeader;
-	IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+	DWORD Signature;								// Sign: "PE\0\0" (0x00004550 i.e. littleend 0x50450000)
+	IMAGE_FILE_HEADER FileHeader;					// Physical file information
+	IMAGE_OPTIONAL_HEADER64 OptionalHeader;			// Memory loading information
 } IMAGE_NT_HEADERS64;
 
+/**
+ * @brief DLL Section
+ * 
+ * @note in .dll file, code have been store as:
+ * 1. .text: machine code (assembly instructions)
+ * 2. .rdata: constant variable
+ * 3. .data: global variable
+ * 
+ */
 typedef struct _IMAGE_SECTION_HEADER
 {
 	BYTE Name[8];
 	DWORD VirtualSize;
-	DWORD VirtualAddress;
+	DWORD VirtualAddress;							// Where in memory (mapped_base) should this segment be moved
 	DWORD SizeOfRawData;
-	DWORD PointerToRawData;
+	DWORD PointerToRawData;							// The location of this segment in the hard drive file (raw_data)
 	DWORD PointerToRelocations;
 	DWORD PointerToLinenumbers;
 	WORD NumberOfRelocations;
@@ -130,8 +139,8 @@ typedef struct _IMAGE_EXPORT_DIRECTORY
 // =========================================================================
 
 // Thunk for: int function(int a, int b)
-// Linux passes: RDI (a), RSI (b)
-// Windows expects: RCX (a), RDX (b)
+// Linux: RDI, RSI, RDX, RCX, R8, R9
+// Windows: RCX(a), RDX(b), R8, R9
 extern "C" int CallWin64Func_2Args(void* func_ptr, int arg1, int arg2)
 {
 	int result = 0;
@@ -139,7 +148,7 @@ extern "C" int CallWin64Func_2Args(void* func_ptr, int arg1, int arg2)
 		"sub $32, %%rsp \n"						// [Win64 ABI] Allocate 32 bytes of shadow space
 		"call *%1 \n"							// Call the Windows function pointer
 		"add $32, %%rsp \n"						// Clean up the shadow space
-		: "=a" (result)							// Output: Return value in EAX mapped to 'result'
+		: "=a" (result)							// Output: Return value in EAX/RAX mapped to 'result'
 		: "r" (func_ptr),						// Input 1 (%1): func_ptr in any general register
 		  "c" (arg1),							// Input 2: Force arg1 into ECX/RCX (Win64 Arg 1)
 		  "d" (arg2)							// Input 3: Force arg2 into EDX/RDX (Win64 Arg 2)
@@ -162,7 +171,8 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	// 1. Read raw DLL file into memory
+	// ----- 1. Read raw DLL file into memory -----
+
 	int fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
@@ -181,19 +191,27 @@ int main(int argc, char** argv)
 	}
 	close(fd);
 
-	// 2. Parse PE Headers
+	// ----- 2. Parse PE Headers -----
+
+	// Read raw file
 	IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)raw_data;
+	// Offset the pointer to NT
 	IMAGE_NT_HEADERS64* nt_headers = (IMAGE_NT_HEADERS64*)(raw_data + dos_header->e_lfanew);
 
-	// 3. Allocate executable memory for the mapped image
+	// ----- 3. Allocate executable memory for the mapped image -----
+
+	// To match the CPU's page alignment (typically 4KB), it will be stretched, thus we use `SizeOfImage`
 	DWORD image_size = nt_headers->OptionalHeader.SizeOfImage;
+	// Allocate memory with `PROT_EXEC`
 	BYTE* mapped_base = (BYTE*)mmap(NULL, image_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	
+
 	printf("[+] Allocated %u bytes of executable memory at %p\n", image_size, mapped_base);
 
-	// 4. Map PE Headers
+	// ----- 4. Map PE Headers -----
+
+	// Copy NT image to mapped_base
 	memcpy(mapped_base, raw_data, nt_headers->OptionalHeader.SizeOfHeaders);
-	
+
 	// Safely calculate Section Header starting point
 	IMAGE_SECTION_HEADER* section = (IMAGE_SECTION_HEADER*)(
 		(BYTE*)nt_headers + 
@@ -202,6 +220,7 @@ int main(int argc, char** argv)
 		nt_headers->FileHeader.SizeOfOptionalHeader
 	);
 	
+	// Traverse all sections
 	printf("[*] Mapping %d Sections...\n", nt_headers->FileHeader.NumberOfSections);
 	for (int i = 0; i < nt_headers->FileHeader.NumberOfSections; i++)
 	{
@@ -222,7 +241,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-	// 5. Parse Export Directory to find function addresses
+	// ----- 5. Parse Export Directory to find function addresses -----
+
 	DWORD export_rva = nt_headers->OptionalHeader.DataDirectory[0].VirtualAddress;
 	if (export_rva == 0)
 	{
@@ -231,13 +251,17 @@ int main(int argc, char** argv)
 	}
 
 	IMAGE_EXPORT_DIRECTORY* export_dir = (IMAGE_EXPORT_DIRECTORY*)(mapped_base + export_rva);
+	// Pointer to function name
 	DWORD* name_rvas = (DWORD*)(mapped_base + export_dir->AddressOfNames);
+	// The index number corresponding to the function name
 	WORD* ordinal_rvas = (WORD*)(mapped_base + export_dir->AddressOfNameOrdinals);
+	// Machine code address (RVA)
 	DWORD* function_rvas = (DWORD*)(mapped_base + export_dir->AddressOfFunctions);
 
 	void* func_GetHelloWorldString = NULL;
 	void* func_CalculateAdd = NULL;
 
+	// Traverse all directory
 	printf("[*] Scanning Export Table (Found %d names)...\n", export_dir->NumberOfNames);
 	for (DWORD i = 0; i < export_dir->NumberOfNames; i++)
 	{
@@ -259,13 +283,16 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// ----- 6. Test Functions -----
+
 	if (func_GetHelloWorldString)
 	{
 		typedef const char* (*GetStrFunc)();
 		GetStrFunc get_str = (GetStrFunc)func_GetHelloWorldString;
 		printf("\n[EXEC] Calling GetHelloWorldString...\n");
 		printf("[OUT]  %s\n", get_str());
-	} else
+	}
+	else
 	{
 		printf("\n[-] Could not find GetHelloWorldString\n");
 	}
@@ -278,7 +305,8 @@ int main(int argc, char** argv)
 		printf("[OUT]  Result: %d\n", res);
 	}
 
-	// Cleanup
+	// ----- 7. Cleanup -----
+
 	munmap(mapped_base, image_size);
 	free(raw_data);
 	printf("\n[*] Done.\n");
