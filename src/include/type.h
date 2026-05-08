0de376afc2dfd59b407b6dcc2608f3ce897fcae6
xjt <969211735@qq.com>
2026-05-08 15:37:14 +0800

    Refactor: change header

---
+++ src/include/type.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint8_t BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+typedef uint64_t QWORD;
+
+#ifdef __cplusplus
+}
+#endif
